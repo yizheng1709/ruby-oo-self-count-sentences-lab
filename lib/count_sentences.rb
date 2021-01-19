6153a6f73229b4d3aed4c35e1e7ddb5a07ad8985
Eva <yi.zheng1709@gmail.com>
2021-01-19 18:40:40 -0500

    commit tree 5ff4fdb65f1de2a2e3468075df24362ca8737973

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false 
   end
 
   def question?
-
+    self[-1] == "?" ? true : false 
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false 
   end
 
   def count_sentences
-
+    self.split(/[.?!] /).count 
   end
+
 end
